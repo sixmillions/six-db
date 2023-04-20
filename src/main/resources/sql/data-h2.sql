01c17a0d3833d15e8dcc7835e4a4eeb1fdae84e5
sixmillions <liubw95@163.com>
2023-04-20 21:30:57 +0800

    feat: 数据库脚本修改

--- src/main/resources/sql/data-h2.sql
+++ src/main/resources/sql/data-h2.sql
@@ -1,5 +1,9 @@
-DELETE
-FROM upload_history;
+insert into app_info (msg)
+values ('app start');
 
 INSERT INTO upload_history (id, file_url, file_name, file_s3_url, file_path, created_by, created_time)
-VALUES (1, 'https://b2.6bw.fun/0/logo.png', 'logo.png', 'xxxx', 'logo.png', 'sys', '2023-04-19 00:00:00');
+SELECT 1, 'https://b2.6bw.fun/0/logo.png', 'logo.png', 'xxxx', 'logo.png', 'sys', '2023-04-19 00:00:00'
+WHERE NOT EXISTS(
+        SELECT 1 FROM upload_history WHERE id = 1
+    );
+
